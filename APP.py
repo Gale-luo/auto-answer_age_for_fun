b5775c494f06e46269199e1b82fc15f3eefa7ae5
YANHAO <60405725+Gale-luo@users.noreply.github.com>
2020-06-18 23:26:07 +0200

    Add files via upload

---
+++ APP.py
@@ -0,0 +1,37 @@
+your_age = input("Enter your age ")
+Her_age = input("Enter Her age ")
+input_1 = your_age
+input_2 = Her_age
+
+
+if int(your_age) > int(Her_age):
+   print ("Because your age is "+your_age+" so you are greater than her")
+elif int(your_age) == int(Her_age):
+   print("Because your age is " + your_age+ " so you and her are equal")
+else:
+   print("Because your age is "+your_age+" so you are less than her")
+
+
+
+
+姓名_1 = input("请输入确认")
+
+
+
+学历 = input ("请输入你的学历")
+print(学历+"稍等系统查询中")
+print("很遗憾"+学历+"暂无法匹配")
+学历_1 =input("请输入你最理想的学历")
+print("匹配通过，你与你的理想"+学历_1+" 又近了一步")
+
+年龄 = input ("请输入你的年龄")
+print(年龄+"是你的年龄正直旺盛之年，要加油哦")
+
+
+
+
+
+
+
+
+
